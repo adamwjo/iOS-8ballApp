2ec5152491e9646cfffeca736e37371634916408
adamwjo <adamwjo@gmail.com>
2019-03-18 13:50:41 -0400

    Initial Commit

---
+++ Stupid 8 Ball App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Stupid 8 Ball App
+//
+//  Created by Adam Johnson on 3/18/19.
+//  Copyright © 2019 Adam Johnson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
